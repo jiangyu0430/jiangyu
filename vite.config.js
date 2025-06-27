fb1d42e2cae9653a8170fd5c8778df59ae84898d
jiangyu0430 <1119274263@qq.com>
2025-06-27 11:58:19 +0800

    vite-base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/jiangyu/',
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.md'], // ✅ 添加对 .md 的支持
   resolve: {
