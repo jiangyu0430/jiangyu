b1e63a392e831c4d1d17be9b47715ef0f0b92aed
jiangyu0430 <1119274263@qq.com>
2025-06-30 15:03:43 +0800

    zhengshiban2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-
 const isGithub = process.env.VITE_DEPLOY_TARGET === 'github'
 
 export default defineConfig({
-  base: isGithub ? '/jiangyu/' : '/', // ← 自动切换
+  base: '/',
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.md'], // ✅ 添加对 .md 的支持
   resolve: {
